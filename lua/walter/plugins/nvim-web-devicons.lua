860512eb89588a2ee6ac7148ffeb0a1e76bee213
Walter Goodzey <145727171+wgoodzey@users.noreply.github.com>
2024-12-12 18:33:14 -0600

    made small change for mac

--- lua/walter/plugins/nvim-web-devicons.lua
+++ lua/walter/plugins/nvim-web-devicons.lua
@@ -7,4 +7,12 @@ devicons.setup({
     color_icons = true,
     default = true,
     strict = true,
+    override_by_operating_system = {
+        ["apple"] = {
+            icon = "",
+            color = "#A2AAAD",
+            cterm_color = "248",
+            name = "Apple",
+        },
+    };
 })
