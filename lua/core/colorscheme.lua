debad6c3f66e11d005c891dcb8be2a3449c85431
Walter Goodzey <145727171+wgoodzey@users.noreply.github.com>
2025-09-03 23:14:10 -0500

    modified hardcoded values for cursor line and ruler

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,34 +1,38 @@
 -- Setup for Catppuccin
 require("catppuccin").setup({
-	flavour = "mocha", -- You can choose between "latte", "frappe", "macchiato", and my favorite "mocha"
-	integrations = {
-		treesitter = true,
-		native_lsp = {
-			enabled = true,
-			virtual_text = {
-				errors = { "italic" },
-				hints = { "italic" },
-				warnings = { "italic" },
-				information = { "italic" },
-			},
-			underlines = {
-				errors = { "underline" },
-				hints = { "underline" },
-				warnings = { "underline" },
-				information = { "underline" },
-			},
-		},
-		lsp_trouble = true,
-		cmp = true,
-		gitsigns = true,
-		telescope = true,
-		nvimtree = true,
-		which_key = true,
-	},
-	custom_highlights = {
-		ColorColumn = { bg = "#242434" },  -- darker
-		CursorLine = { bg = "#313244" },  -- lighter, on top
-	}
+  flavour = "mocha", -- You can choose between "latte", "frappe", "macchiato", and my favorite "mocha"
+  integrations = {
+    treesitter = true,
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+      },
+    },
+    lsp_trouble = true,
+    cmp = true,
+    gitsigns = true,
+    telescope = true,
+    nvimtree = true,
+    which_key = true,
+  },
+  custom_highlights = function(colors)
+    -- require here so it's always in scope when this function runs
+    local u = require("catppuccin.utils.colors")
+    return {
+      ColorColumn = { bg = u.darken(colors.base, 0.85, colors.crust) },
+      CursorLine  = { bg = u.lighten(colors.surface0, 0.95, colors.text) },
+    }
+  end,
 })
 
 -- Apply the theme
